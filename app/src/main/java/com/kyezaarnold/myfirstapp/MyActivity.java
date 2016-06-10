525aafb629145608f1ed3e8ae56fa9d1213291da
Kyeza Arnold <kyezaarnold63@gmail.com>
2016-06-10 10:20:49 +0300

    Adding an Actionbar and customising it

--- app/src/main/java/com/kyezaarnold/myfirstapp/MyActivity.java
+++ app/src/main/java/com/kyezaarnold/myfirstapp/MyActivity.java
@@ -3,6 +3,9 @@ package com.kyezaarnold.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -22,4 +25,26 @@ public class MyActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+//                openSearch();
+                return true;
+            case R.id.action_settings:
+//                openSetting();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
